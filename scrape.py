2e7f3983c40e12f05b2bce034c44c896d40a6e19
lisplesssnake <miruna.ciobanu9@yahoo.com>
2023-07-14 09:35:42 +0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-URL = 'http://localhost/out.html'	# retirejs html output here
+URL = 'http://localhost/out.html'
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find_all("tr",class_="vulnerable")
 
-#print(results.prettify())
+regex = "(?<=Found in)(.*)(?=- Vulnerability info)"
 
 for r in results:
     data = r.find_all("td")
-    print(data[0].text + '\t--\t' + data[1].text)
-    #print(data[1].text)
+    x = re.search(regex,data[2].text)
+    print((data[0].text).ljust(30) + (data[1].text).ljust(30) + (x[0]).ljust(30))
+    print()
